9582b8eb170767cf6bdf020b1ce20ae0e6f7b0cc
Chris <git@maggsweb.co.uk>
2018-07-06 14:33:53 +0100

    Adding comments to index.php

--- index.php
+++ index.php
@@ -1,17 +1,70 @@
 <?php
 
-
+/**
+ * Instantiate the FormValidator for use
+ */
 $formVal = new FormValidator();
+
+/**
+ * Set METHOD.
+ * Default is _GET (in which case this step can be skipped)
+ */
 $formVal->setMethod('POST');
+
+/**
+ * Register all submitted fields into the class
+ */
 $formVal->registerFields();
 
-$formVal->validate('firstname')->clean()->isRequired();
-$formVal->validate('surname')->clean()->isRequired();
-$formVal->validate('emailadd')->clean()->isRequired()->isEmail();
-$formVal->validate('password')->clean()->isRequired()->isPassword();
-$formVal->validate('department')->isRequired();
-$formVal->validate('interests')->checkboxGroupRequired(2);
 
+// ----------------------------------------------------------------
+// Validation Methods  --------------------------------------------
+// ----------------------------------------------------------------
+
+
+/**
+ * Sanitise field
+ * This should be added to all fields that include any sort of user input/selection
+ */
+$formVal->validate('some-field-name')->clean();
+
+/**
+ * Mandatory field
+ * Flag fields as mandatory
+ */
+$formVal->validate('some-field-name')->isRequired();
+
+/**
+ * Validate input value as a valid email address
+ */
+$formVal->validate('some-field-name')->isEmail();
+
+/**
+ * Validate input value as a password, using set rules set
+ */
+$formVal->validate('some-field-name')->isPassword();
+
+/**
+ * Ensure that X number of check-box options have been selected
+ */
+$formVal->validate('some-checkbox-group-name')->checkboxGroupRequired(2);
+
+
+// ----------------------------------------------------------------
+// Process methods  --------------------------------------------
+// ----------------------------------------------------------------
+
+/**
+ * Get an array of error messages in:
+ *  $fieldname => $message
+ * 
+ */
 $errors = $formVal->getErrors();
+
+
+/**
+ * Get an array of cleaned form fields in:
+ *  $fieldname => $value
+ */
 $fields = $formVal->getFields();
 
